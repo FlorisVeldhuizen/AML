885b48c99c9dfeadf91ccd0f52cdeee03ee26754
Floris <flm.veldhuizen@gmail.com>
2019-01-08 16:02:10 +0100

    added sanitazion

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-#matplotlib inline
+# matplotlib inline
 import matplotlib.pyplot as plt
 import pylab
+import numpy as np
 import operator
-
 
 # Import the gym module
 import gym
@@ -14,6 +14,29 @@ frame = env.reset()
 print("the frame variable is of type ", type(frame))
 print("the shape of frame are ", frame.shape)
 
+
+# Img Sanitizing
+def pongcrop(img):
+    print(img)
+    img = img[50:-16, 0:-1, :]
+    return img
+
+
+def grayscale(img):
+    return np.mean(img, axis=2).astype(np.uint8)
+
+
+def downsample(img):
+    img = img[::2, ::2]
+    return img
+
+
+def combosanitize(img):
+    img = pongcrop(img)
+    img = grayscale(img)
+    img = downsample(img)
+    return img
+
 # Render
 env.render()
 rangelimit = 21
@@ -22,8 +45,9 @@ for i in range(rangelimit):
     frame, reward, is_done, _ = env.step(env.action_space.sample())
     # Render
     env.render()
+    frame = combosanitize(frame)
     plt.imshow(frame)
-    if i > rangelimit-1:
+    if i > rangelimit - 1:
         plt.show()
 
 # env.close()  #Add this to stop it from crashing due to sys meta Path is None
